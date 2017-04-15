29388c0fe61ed01778aafe58ad5e26bf05bcc23e
snopboy <mapleglobal.org@gmail.com>
2017-04-16 02:04:46 +0300

    lel

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2016 Or E. (aka Snopboy / Kingslime / CoreModule)
+ *
+ * This program is a non-free PAID software and may not be used and / or
+ * distributed without valid license and written approval by the original author.
+ * See the Software License for more details.
+ *
+ * You should have received a copy of the License
+ * along with this program.  If not, see
+ * <https://www.binpress.com/license/view/l/4fb8be2be60a2357a96b0bfd469bb7aa/>.
+ */
+
+
+/**
+ * Main JavaScript File
+ * All Your Functions Should Go Here
+ */
+ $(function () {
+   $('[data-toggle="tooltip"]').tooltip()
+ });
+ $(function () {
+   $('[data-toggle="buttons"]').button('toggle')
+ });
